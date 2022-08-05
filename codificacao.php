f9963ab50406f221ed3492ede21e845a72851a0a
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-08-05 00:20:04 -0300

    ultilizacao_dos_comandos_necessarios_para_rodar_os_arquivos_'php'_e_comando_de_escrita_'echo'

---
+++ codificacao.php
@@ -0,0 +1,24 @@
+<?php       /* inicio '<?php' e fim '?>' do codigo em php */                                                   
+
+    echo "<font color=blue>PHP</font><br/><br/>";       /* 'echo' comando de escrita faz um print na tela */
+    echo "Hypertext Preprocessor";
+
+?>      
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+
+    <p> Texto escrito em HTML</P>
+
+    <?php       /* pode inserir codigo 'php' em qualquer lugar do documento basta abri a tag '<?php' e fecha-la com ' ?>' */
+        echo "  Texto escrito em PHP";
+    ?>
+</body>
+</html>
