4895cde603bd31474c92a4f950f31478901ef65b
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-08-05 23:09:39 -0300

    realizando_algumas_operacoes_basicas_com_variaveis

---
+++ operacoes com variaveis.php
@@ -0,0 +1,50 @@
+<?php       /* codigo PHP */
+
+    $num1 = 10;
+    $num2 = 3;
+
+                                /* Aritméticos matemáticos */
+    $res = $num1 + $num2;
+    echo "A soma de $num1 com $num2 é : $res <br/>";
+
+    $res = $num1 - $num2;
+    echo "A subtração de $num1 com $num2 é : $res <br/>";
+
+    $res = ($num1 - $num2) * 5;
+    echo "A subtração de $num1 com $num2 e multiplicado por 5 é : $res <br/>";
+
+    $res = $num1 * $num2;
+    echo "$num1 multiplicado por $num2 é: $res <br/>";
+
+    $res = $num1 / $num2;
+    echo "$num1 divido por $num2 é: $res <br/>";
+
+    $res = $num1 % $num2;
+    echo "O resto da divisão de $num1 por $num2 é: $res <hr/>";
+
+                                /* operador de atribuição */
+    $num1 = $num1 + 1;
+    echo "O novo valor de num1 é: $num1 <br/>";
+
+    $num2 += 5;
+    echo "O novo valor do numero num2 é: $num2 <br/>";
+
+                                /* operador de comparação */
+    $res = $num1 == $num2;
+    $res = $num1 != $num2;
+    $res = $num1 > $num2;
+    $res = $num1 < $num2;
+    $res = $num1 >= $num2;
+    $res = $num1 <= $num2;
+
+?>
+
+<!DOCTYPE html>      <!-- codigo HTML -->
+    <head>
+        <meta http-equiv="Content-type" content="text/html; charset=utf-8">    
+        <title>Operações com variáveis</title>
+    </head>
+    <body>
+        
+    </body>
+</html>
