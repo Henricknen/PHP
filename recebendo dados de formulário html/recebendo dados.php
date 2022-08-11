d4188c84dbe35782854a78bc64e35447d0365ac7
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-08-11 15:33:49 -0300

    recebendo_dados_enviados_de_formulario_'html'

---
+++ recebendo dados de formulário html/recebendo dados.php
@@ -0,0 +1,9 @@
+<?php
+
+    $vnome = $_POST["f_nome"];       /* capturando o conteúdo passado pelo formulario via 'get' e armazenado na variável */
+    $vsenha = $_POST["f_senha"];
+
+    echo "Nome: ".$vnome."<br/>Senha: ".$vsenha."<br/>";
+
+?>
+
