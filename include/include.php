39dff97500a881010afff2fee700560afb6edc2b
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-08-13 23:36:55 -0300

    incluindo_conteudo_de_um_arquivo_dentro_de_outro_php_usando_o_comando_'include'

---
+++ include/include.php
@@ -0,0 +1,20 @@
+<?php
+
+    include "data.inc";     /* fazendo a inclusão da data que está no arquivo 'data.inc' */
+    echo "<br/>";
+    include "menu.inc";         /* posso ultilizar 'include' do mesmo arquivo ou de diferente arquivo quantas vezes for necessário */
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Include</title>
+</head>
+<body>
+    
+</body>
+</html>
