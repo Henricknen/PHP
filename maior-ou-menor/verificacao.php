d6a14f944a7556e629e31148174e5a106d9000d3
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-09-10 16:11:38 -0300

    Verificando_se_alor_digitado_e_maior_ou_menor_que_10

---
+++ maior-ou-menor/verificacao.php
@@ -0,0 +1,10 @@
+<?php
+
+    $num = $_POST["num"];
+    if($num > 10) {
+        echo "O numero digitado é maior que 10!";
+    } else {
+        echo "O numero digitado é menor que 10!";
+    }
+
+?>
