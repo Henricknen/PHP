d94d88bda6a72fb72d92b424c8b044dd6f8e1f4a
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-09-14 14:11:19 -0300

    Obtendo_o_quadrado_dos_numeros_15_a_200_com_'while'

---
+++ num_quadrados.php
@@ -0,0 +1,27 @@
+<?PHP
+
+    $i = 15;
+    $tam = 201;
+    $vet = array($tam);
+
+    while($i < $tam) {       
+        $q = $i * $i;
+        echo "O quadradro de " .$i. " é: " .$q, "<br/><br/>";
+        $i++;       
+    }
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>quarados de 25  200</title>
+</head>
+<body>
+    
+</body>
+</html>
