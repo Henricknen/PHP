f2abb11a4ab091d70be02879c90c09deabd460e3
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-09-14 14:32:51 -0300

    criando_soma_dos_100_primeiros_numeros_com_'do_while'

---
+++ soma do while.php
@@ -0,0 +1,24 @@
+<?php
+$i = 0;
+$tam = 101;
+
+    do {
+        $soma = $i;
+        $i++;
+    } while($i < $tam) ;
+    echo "A soma dos numeros é de: $soma<br/>";
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>soma com do while</title>
+</head>
+<body>
+    
+</body>
+</html>
